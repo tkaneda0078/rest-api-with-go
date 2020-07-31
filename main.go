6a3fcc75019c508171a9455b35204728d034edab
金田尚大 <1111510078r21@gmail.com>
2020-07-31 12:24:26 +0900

    パラメータのIDに紐づく本情報を取得する

--- main.go
+++ main.go
@@ -1,18 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
+// Book は本の構造を表現する
+type Book struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Year   string `json:"year"`
+}
+
+var books []Book
+
 func main() {
 	router := mux.NewRouter()
+
+	books = append(books,
+		Book{ID: 1, Title: "一人称単数", Author: "村上 春樹", Year: "2020"},
+		Book{ID: 2, Title: "流浪の月", Author: "凪良 ゆう", Year: "2019"},
+	)
+
 	router.HandleFunc("/books", getBooks).Methods("GET")
+	router.HandleFunc("/books/{id}", getBook).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
-	log.Println("get books")
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	i, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for _, book := range books {
+		if book.ID == i {
+			json.NewEncoder(w).Encode(book)
+		}
+	}
 }
